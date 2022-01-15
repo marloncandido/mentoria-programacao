7b78bcf33ceb24466ff8e5edb9125d59cf661459
Marlon Candido <marlon_08_stz@hotmail.com>
2022-01-15 13:29:22 -0300

    uri beginner simple sum

---
+++ uri/beginner/1003.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+
+    int A, B, SOMA;              // Declaração de variáveis
+    scanf("%d", &A);             // Usuário digita o valor de A
+    scanf("%d", &B);             // Usuário digita o valor de B
+    SOMA = A + B;                // Realiza o cálculo da soma
+    printf("SOMA = %d\n", SOMA); // Retorna o valor para SOMA
+    return 0;
+}
