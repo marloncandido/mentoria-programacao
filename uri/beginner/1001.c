837b0a63c860422729872ad4f830947e465e1621
Marlon Candido <marlon_08_stz@hotmail.com>
2022-01-15 12:35:01 -0300

    uri extremely basic

---
+++ uri/beginner/1001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int A, B, X;            // Declaração de variáveis
+    scanf("%d", &A);        // Recebe um inteiro e armazena em A
+    scanf("%d", &B);        // Recebe um inteiro e armazena em B
+    X = A + B;              // Realiza a soma de A e B
+    printf("X = %d\n", X);  // Retorna o valor para X
+    return 0;
+}
