c19397648bab6ea55c83bc1243e90f7392df136b
Marlon Candido <marlon_08_stz@hotmail.com>
2022-03-26 14:24:33 -0300

    Envio exercicios 1181, 1182 e 1278

--- uri/strings/1278.c
+++ uri/strings/1278.c
@@ -1,24 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 50
 
-#define MAX 100
-
-int main()
+int main(void)
 {
-    int N = 1, Enter = 0;           // Declaração de variáveis inteiras
-    char texto[100];                // Declaração de variável de texto
-    char buffer[MAX];
-    scanf("%d",&N);                 // Usuário digita o número de linhas do texto
-    Enter--;
+    int N = 1, Enter = 0;
+    char frase[51], texto[500];
+    scanf("%d\n", &N);
     while (Enter < N)
     {
-        strcat(buffer,fgets(texto, MAX, stdin));// Programa lê as teclas digitadas pelo usuário
-        if('\n')
+        strcat(texto, fgets(frase, MAX, stdin));
+        if ('\n')
+            Enter++;
+    }
+    
+    char ultimo_era_espaco = 1;
+
+    for (int x = 0; texto[x] != '\0'; x++)
+    {
+        if (texto[x] == ' ')
         {
-            Enter++;                // Soma o número de vezes que o Enter é pressionado
+            if (ultimo_era_espaco)
+            {
+                for (int y = x; texto[y] != '\0'; y++)
+                    texto[y] = texto[y + 1];
+                
+                if (x > 0)
+                    x --;
+                ultimo_era_espaco = 1;
+            }
+            else
+                ultimo_era_espaco = 1;
+        }
+        else
+    	{	
+    		if (texto[x] == '\n')
+                    ultimo_era_espaco = 1;
+    		else
+    		    ultimo_era_espaco = 0;
+    	}
+    }
+    
+    for (int x = 0; texto[x] != '\0'; x++)
+    {
+        if (texto[x] == ' ')
+        {
+            if (texto[x+1] == '\n')
+            {
+                for (int y = x; texto[y] != '\0'; y++)
+                    texto[y] = texto[y + 1];
+                
+                if (x > 0)
+                    x --;
+            }
         }
     }
-    printf("%s\n", buffer);          // Retorna o texto justificado
+    
+    printf("%s", texto);
+    
+    int caracteres = strlen(texto) - N;
+
+    printf("%d", caracteres);
+
     return 0;
 }
